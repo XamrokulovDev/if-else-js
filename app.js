380825eea08982214ad7ee6ba9f2129c0b197c8e
whoamihack1ng <whoamihack1ng@gmail.com>
2024-03-01 19:33:07 +0500

    the project is finished

---
+++ app.js
@@ -0,0 +1,147 @@
+// if else - loop operatorlari!
+
+// 1-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+
+// if(a > 0){
+//     console.log(a+1);
+// }
+// else if(a < 0){
+//     console.log(a);
+// }
+// else{
+//     console.log('NAN');
+// }
+
+// 2-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+
+// if(a > 0){
+//     console.log(a+1);
+// }
+// else if(a < 0){
+//     console.log(a-2);
+// }
+// else{
+//     console.log('NAN');
+// }
+
+// 3-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+
+// if(a > 0){
+//     console.log(a+1);
+// }
+// else if( a < 0){
+//     console.log(a-2);
+// }
+// else if(a === 0){
+//     console.log(10);
+// }
+// else{
+//     console.log('NAN');
+// }
+
+// 4-Misol.
+// 5-Misol.
+
+// 6-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+// const b = prompt('Son kiriting!')*1;
+
+// if(a > b){
+//     console.log(a);
+// }
+// else if(a < b){
+//     console.log(b);
+// }
+// else{
+//     console.log("NAN");
+// }
+
+// 7-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+// const b = prompt('Son kiriting!')*1;
+
+// if(a > b){
+//     console.log(b);
+// }
+// else if(a < b){
+//     console.log(a);
+// }
+// else{
+//     console.log('NAN');
+// }
+
+// 8-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+// const b = prompt('Son kiriting!')*1;
+
+// if(a > b){
+//     console.log(a,b);
+// }
+// else if(a < b){
+//     console.log(b,a);
+// }
+// else{
+//     console.log('NAN');
+// }
+
+// 9-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+// const b = prompt('Son kiriting!')*1;
+
+// if(a < b){
+//     console.log(a,b);
+// }
+// else if(a > b){
+//     console.log(a,b+a);
+// }
+// else{
+//     console.log('NAN');
+// }
+
+// 10-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+// const b = prompt('Son kiriting!')*1;
+
+// if(a < b){
+//     console.log(a,b);
+// }
+// else if(a > b){
+//     console.log(a,b);
+// }
+// else if(a === b){
+//     console.log(0);
+// }
+// else{
+//     console.log('NAN');
+// }
+
+// 11-Misol.
+
+// const a = prompt('Son kiriting!')*1;
+// const b = prompt('Son kiriting!')*1;
+
+// if(a < b){
+//     console.log(b);
+// }
+// else if(a > b){
+//     console.log(a);
+// }
+// else if(a === b){
+//     console.log(0);
+// }
+// else{
+//     console.log('NAN');
+// }
+
+// 12-Misol.
